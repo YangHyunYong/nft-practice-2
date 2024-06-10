0fd615a8d2d15301fc953649aba04cea1341dc0d
YangHyunYong <43340172+YangHyunYong@users.noreply.github.com>
2024-06-10 16:48:30 +0900

    feat: SaleNFT 페이지구현

--- vite/src/pages/SaleNft.tsx
+++ vite/src/pages/SaleNft.tsx
@@ -3,7 +3,6 @@ import { FC, useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { OutletContext } from "../components/Layout";
 import axios from "axios";
-import NftCard from "../components/NftCard";
 import SaleNftCard from "../components/SaleNftCard";
 
 const SaleNft: FC = () => {
